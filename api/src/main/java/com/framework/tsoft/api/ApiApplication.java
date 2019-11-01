10d7ba89df647dd48d674ebc3bf9b922d9000ce4
liupantao <804878782@qq.com>
2019-11-01 14:48:55 +0800

    整合dubbo

---
+++ api/src/main/java/com/framework/tsoft/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.framework.tsoft.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ order/src/main/java/com/framework/tsoft/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.framework.tsoft.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
